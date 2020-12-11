23b851eb5f30ef33176aa636596c3c132fa13999
Alexandr Kozhevnikov <onmotion1@gmail.com>
2020-12-11 21:04:49 +0300

    rm history

--- Pods/Target Support Files/BezierKit/BezierKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BezierKit : NSObject
-@end
-@implementation PodsDummy_BezierKit
-@end
--- Pods/Target Support Files/Pods-magicScrollWheel/Pods-magicScrollWheel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_magicScrollWheel : NSObject
-@end
-@implementation PodsDummy_Pods_magicScrollWheel
-@end
